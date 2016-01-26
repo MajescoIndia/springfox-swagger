2b278e5604d9a8f04379d1c04c45cc69e3bc4db7
biplabnayak <prasad_biplab@yahoo.com>
2016-01-26 23:43:53 +0530

    controller added - WORKING

--- src/main/java/com/majesco/poc/SwaggerPocApplication.java
+++ src/main/java/com/majesco/poc/SwaggerPocApplication.java
@@ -1,12 +1,19 @@
 package com.majesco.poc;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
+@EnableAutoConfiguration
+@Configuration
 @ComponentScan
-public class SwaggerPocApplication {
+public class SwaggerPocApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SwaggerPocApplication.class, args);
--- src/main/java/com/majesco/poc/controller/UserController.java
+++ src/main/java/com/majesco/poc/controller/UserController.java
@@ -3,22 +3,25 @@ package com.majesco.poc.controller;
 import com.majesco.poc.model.User;
 import com.majesco.poc.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Biplab Nayak
  */
 
-@Controller("/user")
+@RestController
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired UserService userService;
 
-    @RequestMapping(path = "/{userId}",method = RequestMethod.GET)
-    public User getUser(@PathVariable("userId")String userId) {
-        return userService.getUser(userId);
+    @RequestMapping(method = RequestMethod.GET)
+    public @ResponseBody
+    User getUser() {
+        return userService.getUser("biplab");
+        //return "dsfbsghdvfgdvsgfvg";
     }
 }
